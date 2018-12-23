b2e80e562d31c7cdc183821b2ba06a63167aaf77
jhetuts <jhetuts@gmail.com>
2018-12-23 18:35:41 +0800

    first upload

---
+++ js/app.js
@@ -0,0 +1,110 @@
+const tasks = [
+    "Feed the dog",
+    "Wash the dishes",
+    "Clean the house",
+    "Water the plants"
+];
+
+const tasksDone = [
+    "Watch NBA",
+    "Check tutorial",
+    "Exercise in the morning"
+]
+
+let tasksWrap = document.querySelector('.list-unstyled'),
+    doneTasksWrap = document.querySelector('#done-items'),
+    inputTask = document.getElementById('input-task'),
+    btnTask = document.getElementById('addTask');
+
+function addTask(tasks){
+    let countTask = document.querySelector(".count-todos");
+    tasksWrap.innerHTML = "";
+    let li = "";
+
+    for(var key in tasks){ 
+        li += `<li class="ui-state-default">
+                <div class="checkbox">
+                    <label>
+                        <input type="checkbox" value="" ban="" data-id="${key}" class="check-item"/>${tasks[key]}
+                    </label>
+                </div>
+            </li>`;
+    }
+    
+    tasksWrap.innerHTML = li;
+    countTask.innerHTML = document.querySelectorAll("ul#sortable li").length;
+
+    checkboxLoader();
+}
+
+function doneTask(taskdone){
+    doneTasksWrap.innerHTML = "";
+    let li = "";
+
+    for(var key in taskdone){
+        li += `
+            <li>${taskdone[key]} 
+                <button class="remove-item btn btn-default btn-xs pull-right" data-id=${key}>
+                    <span class="glyphicon glyphicon-remove"></span>
+                </button>
+            </li>
+        `;
+    }
+    
+    doneTasksWrap.innerHTML = li;
+    removeDoneTasks();
+}
+function addTaskNow(e){
+    if(e.keyCode == 13){
+        if(inputTask.value !== ""){
+            tasks.push(inputTask.value);
+            addTask(tasks);
+        } else {
+            alert('Input task');
+        }
+        inputTask.value = "";
+    }
+}
+function checkTask(id){
+    if(id > -1){
+        tasksDone.push(tasks[id]);
+        tasks.splice(id,1);
+        doneTask(tasksDone);
+        addTask(tasks);
+    }
+    checkboxLoader();
+}
+
+function checkboxLoader(){
+    let checkbox = document.getElementsByClassName('check-item');
+    checkbox = Array.from(checkbox);
+
+    for(let count = 0; count < checkbox.length; count++){
+        checkbox[count].addEventListener('change', function(){
+            if(this.checked){
+                let id = this.dataset.id;
+                checkTask(id);
+            } 
+        });
+    }
+}
+function removeDoneTasks(){
+    let rmvBtn = document.getElementsByClassName('remove-item');
+    rmvBtn = Array.from(rmvBtn);
+
+    console.log(rmvBtn);
+
+    for(let count = 0; count < rmvBtn.length; count++){
+        rmvBtn[count].addEventListener('click', function(){
+            let id = this.dataset.id;
+            tasksDone.splice(id, 1);
+            doneTask(tasksDone);
+        });
+    }
+}
+
+inputTask.addEventListener('keypress', addTaskNow);
+btnTask.addEventListener('keypress', addTaskNow);
+
+addTask(tasks);
+doneTask(tasksDone);
---
+++ readme.txt
@@ -0,0 +1,12 @@
+Simple Add-task Application Using Modern JavaScript
+===================================================
+Version: 1.0
+Publisher: jhetuts
+Date: 6:30 PM 12/23/2018
+
+
+Description:
+    This is only a simple add-to-do / add-task web application using modern JavaScript. It goes with bootstrap css and a read only data.
+
+Purpose:
+    This is just to test my so far knowledge in creating web application using pure Javascript.
